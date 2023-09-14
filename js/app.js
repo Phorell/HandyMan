e8a0f36419c96ec9768335d5a89f5350d7dacf50
Phorell <144713311+Phorell@users.noreply.github.com>
2023-09-14 09:47:49 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -14,6 +14,13 @@
             document.documentElement.classList.add(className);
         }));
     }
+    function addLoadedClass() {
+        if (!document.documentElement.classList.contains("loading")) window.addEventListener("load", (function() {
+            setTimeout((function() {
+                document.documentElement.classList.add("loaded");
+            }), 0);
+        }));
+    }
     function getHash() {
         if (location.hash) return location.hash.replace("#", "");
     }
@@ -322,5 +329,6 @@
     da.init();
     window["FLS"] = true;
     isWebp();
+    addLoadedClass();
     pageNavigation();
 })();
